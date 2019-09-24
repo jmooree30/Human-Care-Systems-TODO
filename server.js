db6f855ee1ccdf5134c949c8575e423bee34cd5a
Jacob Moore <jmoore@softaltern.com>
2019-09-24 10:38:09 -0500

    Adds vue app

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
-const express = require('express');
-const logger = require('morgan');
-const users = require('./routes/users');
-const todos = require('./routes/todos');
-const bodyParser = require('body-parser');
-const mongoose = require('./config/database'); //database configuration
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const logger = require("morgan");
+const users = require("./routes/users");
+const todos = require("./routes/todos");
+const bodyParser = require("body-parser");
+const mongoose = require("./config/database"); //database configuration
+const jwt = require("jsonwebtoken");
 const app = express();
-app.set('secretKey', 'nodeRestApi'); // jwt secret token
+app.set("secretKey", "nodeRestApi"); // jwt secret token
 // connection to mongodb
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-app.use(logger('dev'));
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
+);
+app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.get('/', function (req, res) {
-  res.json({ "server": "Test response" });
+app.get("/", function(req, res) {
+  res.json({ server: "Test response" });
 });
 // public route
-app.use('/users', users);
-app.use('/todos', todos);
+app.use("/users", users);
 // private route
+app.use("/todos", validateUser, todos);
 
 function validateUser(req, res, next) {
-  jwt.verify(req.headers['x-access-token'], req.app.get('secretKey'), function (err, decoded) {
+  jwt.verify(req.headers["x-access-token"], req.app.get("secretKey"), function(
+    err,
+    decoded
+  ) {
     if (err) {
       res.json({ status: "error", message: err.message, data: null });
     } else {
@@ -29,23 +35,21 @@ function validateUser(req, res, next) {
       next();
     }
   });
-
 }
 // express doesn't consider not found 404 as an error so we need to handle 404 explicitly
 // handle 404 error
-app.use(function (req, res, next) {
-  let err = new Error('Not Found');
+app.use(function(req, res, next) {
+  let err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 // handle errors
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   console.log(err);
   if (err.status === 404)
     res.status(404).json({ message: "Page requested not found" });
-  else
-    res.status(500).json({ message: "Internal server error." });
+  else res.status(500).json({ message: "Internal server error." });
 });
-app.listen(3000, function () {
-  console.log('Listening on port 3000');
+app.listen(3000, function() {
+  console.log("Listening on port 3000");
 });
