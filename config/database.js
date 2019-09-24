b91656ecb0aa5324881f4d370f4306b83d128f5d
Jacob Moore <jmoore@softaltern.com>
2019-09-24 09:59:40 -0500

    Use cloud storage for DB and use env vars

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 //Set up mongoose connection
-const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost/node_rest_api';
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const mongoDB = process.env.DB_CONNECTION_STRING;
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
