482a479943a1879d588f347dc27748d86b650fae
Jacob Moore <jmooree30@gmail.com>
2019-09-23 21:34:48 -0500

    Updates server and validation

--- server.js
+++ server.js
@@ -1,13 +1,49 @@
-const express = require('express'); 
+const express = require('express');
 const logger = require('morgan');
+const users = require('./routes/users');
 const bodyParser = require('body-parser');
+const mongoose = require('./config/database'); //database configuration
+const jwt = require('jsonwebtoken');
 const app = express();
-
+app.set('secretKey', 'nodeRestApi'); // jwt secret token
+// connection to mongodb
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.get('/', function(req, res){
- res.json({"server" : "Test response"});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/', function (req, res) {
+  res.json({ "server": "Test response" });
 });
+// public route
+app.use('/users', users);
+// private route
 
-app.listen(3000, function(){ console.log('Listening on port 3000');});
+function validateUser(req, res, next) {
+  jwt.verify(req.headers['x-access-token'], req.app.get('secretKey'), function (err, decoded) {
+    if (err) {
+      res.json({ status: "error", message: err.message, data: null });
+    } else {
+      // add user id to request
+      req.body.userId = decoded.id;
+      next();
+    }
+  });
+
+}
+// express doesn't consider not found 404 as an error so we need to handle 404 explicitly
+// handle 404 error
+app.use(function (req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+// handle errors
+app.use(function (err, req, res, next) {
+  console.log(err);
+  if (err.status === 404)
+    res.status(404).json({ message: "Page requested not found" });
+  else
+    res.status(500).json({ message: "Internal server error." });
+});
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+});
